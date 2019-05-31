adf4ec7b62f1365ac3a942da621adb5f2e77e5b3
C-Anirudh <canirudh16@gmail.com>
2019-05-31 19:50:25 +0530

    used GitHub api to fetch information about user

---
+++ getUsers.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+)
+
+type User struct {
+	Login             string      `json:"login"`
+	ID                int         `json:"id"`
+	AvatarURL         string      `json:"avatar_url"`
+	GravatarID        string      `json:"gravatar_id"`
+	URL               string      `json:"url"`
+	HTMLURL           string      `json:"html_url"`
+	FollowersURL      string      `json:"followers_url"`
+	FollowingURL      string      `json:"following_url"`
+	GistsURL          string      `json:"gists_url"`
+	StarredURL        string      `json:"starred_url"`
+	SubscriptionsURL  string      `json:"subscriptions_url"`
+	OrganizationsURL  string      `json:"organizations_url"`
+	ReposURL          string      `json:"repos_url"`
+	EventsURL         string      `json:"events_url"`
+	ReceivedEventsURL string      `json:"received_events_url"`
+	Type              string      `json:"type"`
+	SiteAdmin         bool        `json:"site_admin"`
+	Name              string      `json:"name"`
+	Company           string      `json:"company"`
+	Blog              string      `json:"blog"`
+	Location          string      `json:"location"`
+	Email             string      `json:"email"`
+	Hireable          interface{} `json:"hireable"`
+	Bio               string      `json:"bio"`
+	PublicRepos       int         `json:"public_repos"`
+	PublicGists       int         `json:"public_gists"`
+	Followers         int         `json:"followers"`
+	Following         int         `json:"following"`
+	CreatedAt         time.Time   `json:"created_at"`
+	UpdatedAt         time.Time   `json:"updated_at"`
+}
+
+const (
+	apiURL       = "https://api.github.com"
+	userEndpoint = "/users/"
+)
+
+func getUserInfo(name string) User {
+	resp, err := http.Get(apiURL + userEndpoint + name)
+	if err != nil {
+		Exit(fmt.Sprintf("Error! Unable to fetch information for user: %s\n", name))
+	}
+	defer resp.Body.Close()
+
+	var user User
+	json.NewDecoder(resp.Body).Decode(&user)
+	return user
+}
+
+func Exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
+}
